efc183f37ad2a222241a66bbd41e983abafb687c
hyperkarmic <randy_pan@yahoo.com>
2025-04-25 19:35:58 +0100

    array randomized and and answer variable created

--- logic.js
+++ logic.js
@@ -19,8 +19,14 @@ const number = Math.trunc(Math.random()*20) + 1;
 
 
 [one,two,three,four] = arr
+const answer = one;
+console.log({answer})
 console.log({arr})
-
+let shuffledArr = arr
+    .map(value => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value)
+console.log({shuffledArr})
 eins.innerHTML=`${one}`
 zwei.innerHTML=`${two}`
 drei.innerHTML=`${three}`
